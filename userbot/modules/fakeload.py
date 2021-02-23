998404172265efc2c046781c055d16c2a1f86403
ronaldyganteng <77110439+ronaldyganteng@users.noreply.github.com>
2021-02-23 14:22:10 +0700

    Update fakeload.py

--- userbot/modules/fakeload.py
+++ userbot/modules/fakeload.py
@@ -7,7 +7,7 @@ from userbot.events import register
 @register(outgoing=True, pattern=r"^\.kill(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Mau Gue Santet Lu Broo?..`")
+    await typew.edit("`Mau Aku Pelet Ndak Kamu?!..`")
     sleep(4)
     await typew.edit("0%")
     number = 1
@@ -310,13 +310,13 @@ async def typewriter(typew):
     sleep(0.03)
     await typew.edit(str(number) + "%   ████████████████▌")
     sleep(1)
-    await typew.edit("Tapi Boong......:v")
+    await typew.edit("Tapi Boong Kaga Jadi Pelet Kamu......:v")
     # I did it for two hours :D just ctrl+c - crtl+v
 
 
 CMD_HELP.update(
     {
         "fakeload": ".kill\
-        \nUsage: Gagal Santet."
+        \nUsage: Gagal Pelet Kamu!."
     }
 )
